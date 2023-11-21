f1443b6ccb68b72f82fc7284c5aa7df0430752c6
wwcq <1514320887@qq.com>
2023-11-21 16:10:44 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>
+      {{ msg }}
+    </h2>
+      
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      msg:'Hello github'
+    }
+  },
+  // async created(){
+  //   const result = await axios({
+  //     url: 'http://localhost:8080/hello',
+  //     method:'GET'
+  //   })
+  //   this.msg = result.data.obj.msg
+  // }
+}
+</script>
